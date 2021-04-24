8e6f60539c80a09b9962978029ac007172be58d3
Alan Goodier <alangoodier@gmail.com>
2021-04-24 16:01:46 +0100

    first_commit

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const router = require('express').Router(); 
+
+router.post('/register', () => {
+    res.send("Register")
+});
+
+// router.post('/login', () = {})
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// import routes
+const authRoute = require('./routes/auth');
+
+// route middlewares
+app.use('api/user', authRoute);
+
+
+
+
+
+app.listen(3000, () => console.log('Server up and running'));
